74b2064f686c0ac9048731d2eb390db377f9db5b
gshitole <gshitole@pulsepoint.com>
2022-10-31 12:04:24 +0530

    Task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+// import Body from './components/Body/Body';
+import Courses from './components/Courses/Courses';
+import { mockedAuthorsList, mockedCoursesList } from './constants';
+import CreateCourse from './components/createCourse/createCourse';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [addNewCourseSection, toggleClick] = useState(false);
+	const [AuthorsList, updateAuthorsList] = useState(mockedAuthorsList);
+	const [CoursesList, updateCoursesList] = useState(mockedCoursesList);
+
+	// function toggleCourseSection() {
+	// 	return (
+	// 		<div className='border border-primary rounded'>
+	// 			{
+	// 				if(addNewCourseSection){
+	// 				<CreateCourse authorList={mockedAuthorsList}></CreateCourse>} else
+	// 				{
+	// 					<div className='border border-primary rounded'>
+	// 						<Courses
+	// 							courseList={mockedCoursesList}
+	// 							authorList={mockedAuthorsList}
+	// 							addNewCourseClick={(addNewCourseSection = true)}
+	// 						></Courses>
+	// 					</div>
+	// 				}
+	// 			}
+	// 		</div>
+	// 	);
+	// }
+
+	function setAuthorList(val) {
+		updateAuthorsList((oldArray) => [val, ...oldArray]);
+	}
+
+	function setCourseList(val) {
+		updateCoursesList((oldArray) => [val, ...oldArray]);
+	}
+
+	function toggleCourseSection() {
+		if (addNewCourseSection) {
+			return (
+				<CreateCourse
+					authorList={[...AuthorsList]}
+					courseList={[...CoursesList]}
+					setAuthorsList={(e) => setAuthorList(e)}
+					addNewCourseClick={() => toggleClick(false)}
+					setCourseList={(e) => setCourseList(e)}
+				></CreateCourse>
+			);
+		} else {
+			return (
+				<Courses
+					courseList={[...CoursesList]}
+					authorList={AuthorsList}
+					addNewCourseClick={() => toggleClick(true)}
+				></Courses>
+			);
+		}
+	}
+
+	return (
+		<div className='container-fluid'>
+			<Header></Header>
+			<div className='border border-primary rounded'>
+				{toggleCourseSection()}
+			</div>
+			{/* <Body></Body> */}
+		</div>
+	);
 }
 
 export default App;
